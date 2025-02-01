8d773aca2886b7fcdee346d796d611503e77113f
Jayachandran <159675826+jai3747@users.noreply.github.com>
2025-02-01 10:50:23 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -2,36 +2,36 @@ export const backend_API = "http://34.100.172.207:5000";
 // global.js
 
 
-export const API = {
-  // Movies
-  getAllMovies: () => fetch(`${backend_API}/api/movies`),
-  addMovie: (movie) => fetch(`${backend_API}/api/movies/add-movie`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(movie)
-  }),
-  updateMovie: (id, movie) => fetch(`${backend_API}/api/movies/edit-movie/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(movie)
-  }),
-  deleteMovie: (id) => fetch(`${backend_API}/api/movies/delete-movie/${id}`, {
-    method: 'DELETE'
-  }),
+// export const API = {
+//   // Movies
+//   getAllMovies: () => fetch(`${backend_API}/api/movies`),
+//   addMovie: (movie) => fetch(`${backend_API}/api/movies/add-movie`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(movie)
+//   }),
+//   updateMovie: (id, movie) => fetch(`${backend_API}/api/movies/edit-movie/${id}`, {
+//     method: 'PUT',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(movie)
+//   }),
+//   deleteMovie: (id) => fetch(`${backend_API}/api/movies/delete-movie/${id}`, {
+//     method: 'DELETE'
+//   }),
 
-  // Actors
-  getAllActors: () => fetch(`${backend_API}/api/actors`),
-  addActor: (actor) => fetch(`${backend_API}/api/actors/add-actor`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(actor)
-  }),
+//   // Actors
+//   getAllActors: () => fetch(`${backend_API}/api/actors`),
+//   addActor: (actor) => fetch(`${backend_API}/api/actors/add-actor`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(actor)
+//   }),
 
-  // Producers
-  getAllProducers: () => fetch(`${backend_API}/api/producers`),
-  addProducer: (producer) => fetch(`${backend_API}/api/producers/add-producer`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(producer)
-  })
-};
+//   // Producers
+//   getAllProducers: () => fetch(`${backend_API}/api/producers`),
+//   addProducer: (producer) => fetch(`${backend_API}/api/producers/add-producer`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(producer)
+//   })
+// };
