7708fa81f43b044626be5815524ec1ee5317e141
Jayachandran <159675826+jai3747@users.noreply.github.com>
2025-02-14 00:42:20 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-export const backend_API = "http://35.200.248.177:5000/api";
+export const backend_API = "http://34.47.248.179:5000/api";
 
 // export const backend_API = "http://backend.awsaparna123.xyz/:5000/api";
 
